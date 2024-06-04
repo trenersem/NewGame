263d0a25273a76547c6ced7aafafae59e7898167
Semenov Volodymyr <trenersem1998@gmail.com>
2024-06-03 23:21:52 -0400

    start workink on BEntoGrid

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 export default function Home() {
   return (
       <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
          <div className="max-w-7xl w-full">
+           {/* <FloatingNav /> */}
            <Hero />
+           <Grid />
            
          </div>
       </main>
