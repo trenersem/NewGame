cd4015886308104bc8f722290b58dc9fbe910bca
Semenov Volodymyr <trenersem1998@gmail.com>
2024-06-07 19:04:06 -0400

    safari fixes

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 
 const MagicButton = ({
@@ -6,7 +8,11 @@ const MagicButton = ({
     title: string, icon: React.ReactNode, position: string,
     handleClick?: () => void, otherClasses?: string
 }) => {
-  return (
+    function isSafari() {
+    const ua = navigator.userAgent;
+    return ua.includes("Safari") && !ua.includes("Chrome");
+}
+  return !isSafari() ? (
     <button
         className="relative inline-flex h-12 overflow-hidden rounded-lg w-full p-[1px] focus:outline-none md:w-60 md:mt-10 "
         onClick={handleClick}
@@ -20,6 +26,23 @@ const MagicButton = ({
            {title}
            {position === 'right' && icon}
         </span>
+    </button> 
+  ) : (
+      <button className="bg-slate-800  md:w-60 md:mt-10 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+        <span className="absolute inset-0 overflow-hidden rounded-full">
+            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+        </span>
+        <div className="relative flex h-12 space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+            <span 
+            className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2
+            ${otherClasses}`}
+            >
+                {position === 'left' && icon}
+                {title}
+                {position === 'right' && icon}
+            </span>
+        </div>
+        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
     </button>
   )
 }
